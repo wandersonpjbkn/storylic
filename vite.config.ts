584ea72b4df627c554196df9cd42e3d6c4e20b30
Wanderson PJ <wandersonpj_bkn@hotmail.com>
2025-11-13 20:50:17 -0300

    chore: adds PWA feature

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,54 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss(), process.env.NODE_ENV === 'development' && vueDevTools()],
+  plugins: [
+    vue(),
+    tailwindcss(),
+    vueDevTools(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+      },
+      devOptions: {
+        enabled: true,
+      },
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Storylic',
+        short_name: 'Storylic',
+        description: 'Crie histórias incríveis com cartas aleatórias!',
+        theme_color: '#8b5cf6',
+        background_color: '#e64893',
+        icons: [
+          {
+            src: 'android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'any',
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'maskable',
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
