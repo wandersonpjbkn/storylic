6341a8d4d5599be141b6cee55e6fbb8f262c3503
Wanderson PJ <wandersonpj_bkn@hotmail.com>
2025-11-13 02:40:11 -0300

    fix: removes devTools from prod

--- vite.config.ts
+++ vite.config.ts
@@ -7,14 +7,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-    tailwindcss()
-  ],
+  plugins: [vue(), tailwindcss(), process.env.NODE_ENV === 'development' && vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
   css: {
